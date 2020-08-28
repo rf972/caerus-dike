ef0f78dc715616ff72ee1bcf164a370a6ff9c3fc
Peter Puhov <peter.puhov@linaro.org>
2020-08-28 13:40:31 -0400

    Functional prototype

--- minio/build_server.sh
+++ minio/build_server.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -d build ]; then
+  mkdir build
+fi
+
 docker run \
     --mount type=bind,source="$(pwd)"/minio,target=/minio \
     --mount type=bind,source="$(pwd)"/build,target=/build \
--- minio/run_server.sh
+++ minio/run_server.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -d data ]; then
+  mkdir data
+fi
+
 docker run -p 9000:9000 \
   -e "MINIO_ACCESS_KEY=admin" \
   -e "MINIO_SECRET_KEY=admin123" \
