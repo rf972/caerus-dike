27a884dd56ef18c2f8a4b69c8a9fdd5932ca5cec
Peter Puhov <peterpuhov.github@gmail.com>
2020-09-09 10:45:32 -0400

    Added minio-go mount

--- mc/build_mc.sh
+++ mc/build_mc.sh
@@ -10,6 +10,7 @@ chmod a+x ./build/docker-entrypoint.sh
 docker run \
     --mount type=bind,source="$(pwd)"/mc,target=/mc \
     --mount type=bind,source="$(pwd)"/build,target=/build \
+    --mount type=bind,source="$(pwd)"/minio-go,target=/minio-go \
     mc_build
 
     
