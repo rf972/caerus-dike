ee41efa1ea5285216861414650904263006681a9
Peter Puhov <peterpuhov.github@gmail.com>
2020-09-09 14:20:27 -0400

    Tracing support

--- mc/docker/build.sh
+++ mc/docker/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+cd /minio-go
+go install -x -v -v -ldflags "$(go run /minio-go/buildscripts/gen-ldflags.go)"
+
 cd /mc
 go install -x -v -v -ldflags "$(go run /mc/buildscripts/gen-ldflags.go)"
 
