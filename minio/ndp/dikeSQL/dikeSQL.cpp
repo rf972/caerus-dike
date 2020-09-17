ce9c35c7dab6a7f1dfd66e5d29327e82854c9905
Peter Puhov <peterpuhov.github@gmail.com>
2020-09-17 11:50:50 -0400

    Added rapixml parser

--- minio/ndp/dikeSQL/dikeSQL.cpp
+++ minio/ndp/dikeSQL/dikeSQL.cpp
@@ -10,6 +10,8 @@
 #include <httpparser/request.h>
 #include <httpparser/httprequestparser.h>
 
+#include "rapidxml.hpp"
+
 
 static int db_callback(void* data, int argc, char** argv, char** azColName);
 
@@ -33,13 +35,29 @@ int main (int argc, char *argv[])
     httpparser::HttpRequestParser parser;
     httpparser::HttpRequestParser::ParseResult res = parser.parse(request, pipeInput.c_str(), pipeInput.c_str() + pipeInput.length());
     if( res == httpparser::HttpRequestParser::ParsingCompleted ) {
-        std::cout << request.inspect() << std::endl;
-        return 0;
+        std::cout << request.inspect() << std::endl;        
     } else {
         std::cerr << "Parsing failed" << std::endl;
         return 1;
     }
-    
+
+
+	rapidxml::xml_document<> doc;
+	rapidxml::xml_node<> * root_node;
+	// Read the xml file into a vector
+	//ifstream theFile ("beerJournal.xml");
+	//vector<char> buffer((istreambuf_iterator<char>(theFile)), istreambuf_iterator<char>());
+    request.content.push_back('\0');
+	//buffer.push_back('\0');
+	// Parse the buffer using the xml file parsing library into doc 
+	doc.parse<0>(&request.content[0]);
+	// Find our root node
+	root_node = doc.first_node("SelectObjectContentRequest");
+    //for (xml_node<> * node = root_node->first_node("Brewery"); brewery_node; brewery_node = brewery_node->next_sibling())
+    std::cout << root_node->first_node()->name() << std::endl;
+    rapidxml::xml_node<> * expression_node = root_node->first_node("Expression");
+    std::cout << expression_node->value() << std::endl;
+
     return 0;
     /*
     std::string query = ("SELECT Location, PopTotal, PopDensity, Time  FROM temp.t1 "
