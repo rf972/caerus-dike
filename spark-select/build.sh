395393f767f852a4341e451d6ccdbf48740902fd
Robert Foley <robfoley972@gmail.com>
2020-09-30 15:03:28 -0400

    Change spark-select to build with new docker.

--- spark-select/build.sh
+++ spark-select/build.sh
@@ -4,9 +4,9 @@ if [ ! -d build ]; then
   mkdir build
 fi
 
-docker run --rm -it --name spark_select_build \
+docker run --rm -it --name spark-select-build \
     --mount type=bind,source="$(pwd)"/spark-select,target=/spark-select \
     --mount type=bind,source="$(pwd)"/build,target=/build \
-    spark_build plugin
+    spark_build spark-select
 
     
