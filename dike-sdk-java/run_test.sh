fff11fd741930594fed0412950f013c638d54c70
Peter Puhov <peterpuhov.github@gmail.com>
2020-10-09 09:51:03 -0400

    Removed bin files

--- dike-sdk-java/run_test.sh
+++ dike-sdk-java/run_test.sh
@@ -9,7 +9,7 @@ docker run -it --rm  \
 -e "AWS_ACCESS_KEY_ID=admin" \
 -e "AWS_SECRET_ACCESS_KEY=admin123" \
 maven:3.6.3-jdk-8 java -Xmx1g org.dike.s3.SelectObjectContent 'tpch-test' 'lineitem.csv' \
-'SELECT s.l_orderkey,s.l_partkey,s.l_suppkey,s.l_linenumber,s.l_quantity,s.l_extendedprice,s.l_discount,s.l_tax,s.l_returnflag,s.l_linestatus,s.l_shipdate,s.l_commitdate,s.l_receiptdate,s.l_shipinstruct,s.l_shipmode,s.l_comment FROM (SELECT * FROM S3Object LIMIT 4 OFFSET 1000001) s' 
+'SELECT s.l_orderkey,s.l_partkey,s.l_suppkey,s.l_linenumber,s.l_quantity,s.l_extendedprice,s.l_discount,s.l_tax,s.l_returnflag,s.l_linestatus,s.l_shipdate,s.l_commitdate,s.l_receiptdate,s.l_shipinstruct,s.l_shipmode,s.l_comment FROM (SELECT * FROM S3Object LIMIT 4 OFFSET 5000000) s' 
 
 #'SELECT s.l_orderkey FROM (SELECT * FROM S3Object LIMIT 2 OFFSET 1) AS s'
 #'SELECT s.l_orderkey,s.l_partkey,s.l_suppkey,s.l_linenumber,s.l_quantity,s.l_extendedprice,s.l_discount,s.l_tax,s.l_returnflag,s.l_linestatus,s.l_shipdate,s.l_commitdate,s.l_receiptdate,s.l_shipinstruct,s.l_shipmode,s.l_comment FROM (SELECT * FROM S3Object LIMIT 2 OFFSET 1) AS s'
