6e8797f12002b4beb3a742c11cebfc12df8e0961
Robert Foley <robfoley972@gmail.com>
2020-10-13 11:23:14 -0400

    Updated with changes to our tpch scripts from testing.

--- benchmark/tpch/build_tpch.sh
+++ benchmark/tpch/build_tpch.sh
@@ -3,7 +3,7 @@
 if [ "$1" == "debug" ]; then
   echo "Debugging"
   shift
-  docker run --rm -it --name tpch_build \
+  docker run --rm -it --name tpch_build_debug \
     --mount type=bind,source="$(pwd)"/../tpch,target=/tpch \
     --mount type=bind,source="$(pwd)"/../../spark,target=/spark \
     --entrypoint /bin/bash -w /tpch/tpch-spark \
--- mc/examples/mc_tpch_test.sh
+++ mc/examples/mc_tpch_test.sh
@@ -5,7 +5,7 @@ SOURCE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT="$SOURCE/.."
 
 
-for f in nation region supplier customer part partsupp orders lineitem
+for f in nation region supplier customer part partsupp order lineitem
 do
     echo "$f.csv records:"
     $ROOT/run_mc.sh minio sql --csv-input "rd=\r,fh=USE,fd=," \
