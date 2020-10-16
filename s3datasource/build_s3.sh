9eac842ad3491126bb222be9765144c36058551c
Robert Foley <robfoley972@gmail.com>
2020-10-16 14:35:41 -0400

    Improved the s3datasource build script so that we can avoid downloading extra dependencies. Also cleaned up an extra file import, which could cause the build to fail.

--- s3datasource/build_s3.sh
+++ s3datasource/build_s3.sh
@@ -3,10 +3,9 @@ if [ "$#" -gt 0 ]; then
   if [ "$1" == "debug" ]; then
     echo "Debugging"
     shift
-    echo "build with:   sbt --ivy /s3datasource/build/ivy/"
+    echo "build with:   sbt"
     docker run --rm -it --name s3_build_debug \
       --mount type=bind,source="$(pwd)"/../s3datasource,target=/s3datasource \
-      --mount type=bind,source="$(pwd)"/../../spark,target=/spark \
       --entrypoint /bin/bash -w /s3datasource/s3datasource\
       spark_build 
   fi
@@ -14,7 +13,6 @@ else
   echo "Building s3datasource"
   docker run --rm -it --name s3_build \
     --mount type=bind,source="$(pwd)"/../s3datasource,target=/s3datasource \
-    --mount type=bind,source="$(pwd)"/../../spark,target=/spark \
     --entrypoint /s3datasource/build/build.sh -w /s3datasource/s3datasource \
     spark_build
 fi
