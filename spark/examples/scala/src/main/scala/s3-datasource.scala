c23ec283debcac82828e28063e299fe64af9f084
Robert Foley <robfoley972@gmail.com>
2020-11-20 17:07:13 -0500

    Added more examples to integer test.

--- spark/examples/scala/src/main/scala/s3-datasource.scala
+++ spark/examples/scala/src/main/scala/s3-datasource.scala
@@ -13,6 +13,9 @@ import org.apache.spark.sql.functions.sum
 import org.apache.spark.sql.functions.avg
 import org.apache.spark.sql.functions.udf
 import org.scalatest.Assertions._
+import org.apache.spark.sql.catalyst.ScalaReflection
+import org.apache.spark.sql.{Dataset, Row}
+import scala.reflect.runtime.universe._
 
 import org.apache.spark.sql.Row
 
@@ -22,10 +25,63 @@ object S3DatasourceTests {
   }
   def checkResult(expected: String, received: String) {
     if (expected != received) {
-      throw new RuntimeException("expected: " + expected + " != received: " + received)
+      println("expected: " + expected + " != received: " + received)
+      throw new RuntimeException("expected: != received: ")
     }
     println("RESULT: " + expected)
   }
+
+
+
+  def hadoopS3ConnectorExample(args: Array[String]) { 
+    if (args.length == 0) {
+        println("missing arg for s3 ip addr") 
+        System.exit(1)
+    }
+    val s3IpAddr = args(0)
+  
+    val schema = new StructType()
+       .add("i",IntegerType,true)
+       .add("j",IntegerType,true)
+       .add("k",IntegerType,true)
+    val sparkSession = SparkSession.builder
+      .master("local[2]")
+      .appName("example")
+      .getOrCreate()
+
+    sparkSession.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", "admin")
+    sparkSession.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", "admin123")
+    sparkSession.sparkContext.hadoopConfiguration.set("fs.s3a.endpoint", 
+                                                s"""http://$s3IpAddr:9000""")
+    sparkSession.sparkContext.hadoopConfiguration.set("fs.s3a.path.style.access", "true")
+    sparkSession.sparkContext.hadoopConfiguration.set("fs.s3a.impl", 
+                                        "org.apache.hadoop.fs.s3a.S3AFileSystem")
+
+    // sparkSession.sparkContext.setLogLevel("TRACE")
+
+    import sparkSession.implicits._
+
+    var start = System.currentTimeMillis()
+    val df = sparkSession.read
+      //.schema(schema)
+      .options(Map("delimiter"->",","header"->"true","inferSchema"->"true"))
+      .option("quote", "\"")
+      .csv("s3a://tpch-test/lineitem.csv")
+      .repartition(1)
+      //.csv("s3a://spark-test/ints.csv")
+      
+    println("count is: " + df.count())
+    var end = System.currentTimeMillis()
+
+    var seconds = (end - start) / 1000.0
+    println("Count Time " + seconds)
+    start = System.currentTimeMillis()
+    df.show()
+    end = System.currentTimeMillis()
+    seconds = (end - start) / 1000.0
+    println("Show Time " + seconds)
+  }
+
   def integerExample(args: Array[String]) { 
     if (args.length == 0) {
         println("missing arg for s3 ip addr") 
@@ -44,7 +100,7 @@ object S3DatasourceTests {
       .config("spark.datasource.s3.accessKey", "admin")
       .config("spark.datasource.s3.secretKey", "admin123")
       .getOrCreate()
-    // sparkSession.sparkContext.setLogLevel("TRACE")
+    //sparkSession.sparkContext.setLogLevel("TRACE")
 
     import sparkSession.implicits._
 
@@ -53,10 +109,69 @@ object S3DatasourceTests {
       .format("com.github.s3datasource")
       .schema(schema)
       .option("format", "csv")
-      .load("s3a://spark-test/s3_ints_3.tbl")
+      .load("s3a://spark-test/s3_ints.tbl")
+    df.createOrReplaceTempView("integers")
 
-    //query.show()    
+    //sparkSession.sql("SELECT count(*) FROM integers").show()
+    //df.show()    
     //query.explain("extended")
+    
+    checkRowResult(Seq(Row(5, 12), Row(10, 9)).mkString(","),
+                   df.groupBy("j").agg(sum("i")).collect.mkString(","))
+    checkRowResult(Seq(Row(12), Row(9)).mkString(","),
+                   sparkSession.sql("SELECT sum(i) FROM integers GROUP BY j").collect.mkString(","))
+    checkRowResult(Seq(Row(5, 12), Row(10, 9)).mkString(","),
+                   sparkSession.sql("SELECT j, sum(i) FROM integers GROUP BY j").collect.mkString(","))
+    checkRowResult(Seq(Row(12, 5), Row(9, 10)).mkString(","),
+                   sparkSession.sql("SELECT sum(i), j FROM integers GROUP BY j").collect.mkString(","))
+    checkRowResult(Seq(Row(4, 5, 12, 1), Row(6, 10, 9, 2)).mkString(","),
+                   sparkSession.sql("SELECT sum(k), j, sum(i), min(k) FROM integers GROUP BY j")
+                               .collect.mkString(","))
+    var q1 = sparkSession.sql("SELECT sum(i), j, sum(k), min(k) FROM integers WHERE i > 1" +
+                                    " GROUP BY j")
+    q1.explain(true)
+    q1.show()
+    q1 = sparkSession.sql("SELECT sum(k), j, sum(i), min(k) FROM integers WHERE i > 1" +
+                                    " GROUP BY j")
+    q1.explain(true)
+    q1.show()
+    checkRowResult(Seq(Row(12, 5, 3, 1), Row(8, 10, 4, 2)).mkString(","),
+                   sparkSession.sql("SELECT sum(i), j, sum(k), min(k) FROM integers WHERE i > 1" +
+                                    " GROUP BY j")
+                               .collect.mkString(","))
+     checkRowResult(Seq(Row(5, 15),Row(10, 40)).mkString(","),
+                   sparkSession.sql("SELECT j, sum(k * j) FROM integers WHERE i > 1" +
+                                    " GROUP BY j")
+                               .collect.mkString(","))
+     checkRowResult(Seq(Row(15, 5),Row(40, 10)).mkString(","),
+                   sparkSession.sql("SELECT sum(k * j), j FROM integers WHERE i > 1" +
+                                    " GROUP BY j")
+                               .collect.mkString(","))
+
+    /* group by multiple j,k */
+
+     /*checkRowResult(Seq(Row(1, 15, 5, 1),Row(2, 40, 10, 2)).mkString(","),
+                   sparkSession.sql("SELECT k, sum(k * j), j, k FROM integers WHERE i > 1" +
+                                    " GROUP BY j, k")
+                               .collect.mkString(",")) */
+
+    /* filter of groups "HAVING" */
+    
+     checkRowResult(Seq(Row(15),Row(40)).mkString(","),
+                   sparkSession.sql("SELECT sum(k * j) FROM integers WHERE i > 1" +
+                                    " GROUP BY j")
+                               .collect.mkString(","))
+     checkRowResult(Seq(Row(5, 6),Row(10, 18)).mkString(","),
+                   sparkSession.sql("SELECT j, sum(i * k) FROM integers WHERE i != 6" +
+                                    " GROUP BY j")
+                               .collect.mkString(","))                               
+    checkRowResult(Seq(Row(18),Row(24)).mkString(","),
+                   sparkSession.sql("SELECT sum(k + j) FROM integers WHERE i > 1" +
+                                    " GROUP BY j")
+                               .collect.mkString(","))
+    checkRowResult(Seq(Row(165)).mkString(","),
+                   df.filter("i > 4")
+                     .agg(sum("i") * sum("j")).collect.mkString(",")) 
     checkRowResult(Seq(Row(50)).mkString(","),
                    df.agg(sum("j")).collect.mkString(","))
     checkRowResult(Seq(Row(1, 2)).mkString(","),
@@ -64,6 +179,9 @@ object S3DatasourceTests {
     checkRowResult(Seq(Row(15, 5, 10, 7.5)).mkString(","),
                    df.filter("i > 4")
                      .agg(sum("j"), min("j"), max("j"), avg("j")).collect.mkString(","))
+    checkRowResult(Seq(Row(15, 5, 7.5, 10)).mkString(","),
+                   df.filter("i > 4")
+                     .agg(sum("j"), min("j"), avg("j"), max("j")).collect.mkString(","))
     checkRowResult(Seq(Row(21, 0, 6, 3.0)).mkString(","),
                    df.agg(sum("i"), min("i"), max("i"), avg("i")).collect.mkString(","))
     checkRowResult(Seq(Row(0, 5, 1), Row(1, 10, 2), Row(2, 5, 1),
@@ -73,14 +191,11 @@ object S3DatasourceTests {
                        Row(3, 10), Row(4, 5), Row(5, 10), Row(6, 5)).mkString(","),
                    df.select("i","j").collect.mkString(","))
     checkRowResult(Seq(Row(5,10,2),Row(6,5,1)).mkString(","),
-                   df.filter("i >= 5").collect.mkString(","))
-    checkResult("1", df.filter("i == 0").count().toString)
-    //df.groupBy("j").agg(sum("i"),sum("j"),sum("k")).explain("extended")
-    //df.groupBy("j").agg(sum("i"),sum("j"),sum("k")).show()
+                   df.filter("i >= 5").collect.mkString(",")) 
+    //checkResult("1", df.filter("i == 0").count().toString)
     //df.groupBy("j").agg(count($"i")).show()
-    // df.filter("i > 3").groupBy("j").agg(sum($"i")).where(sum("i") > 10)
     sparkSession.stop()
-  }  
+  }
   def peopleExample(args: Array[String]) { 
     if (args.length == 0) {
         println("missing arg for s3 ip addr") 
@@ -164,8 +279,9 @@ object S3DatasourceTests {
   }
 
   def main(args: Array[String]) { 
-
+    //hadoopS3ConnectorExample(args)
     integerExample(args)
+    //lineItemExample(args)
     //peopleExample(args)
   }
 }
