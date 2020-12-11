aee1af88ffd1b5991edfb1bd39346a4594c1c677
Robert Foley <robfoley972@gmail.com>
2020-12-11 16:04:22 -0500

    Added h2 as dependency to tpch test. Also modified debug mode to also use extra jars.

--- benchmark/tpch/run_tpch.sh
+++ benchmark/tpch/run_tpch.sh
@@ -12,12 +12,13 @@ if [ "$1" == "debug" ]; then
   --conf "spark.driver.maxResultSize=20g" \
   --conf "spark.driver.memory=20g" \
   --conf "spark.driver.extraJavaOptions=-classpath /conf/:/build/spark-3.1.0/jars/*:/examples/scala/target/scala-2.12/ -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=172.18.0.3:5005" \
-    --packages com.github.scopt:scopt_2.12:4.0.0-RC2,com.amazonaws:aws-java-sdk:1.11.853,org.apache.hadoop:hadoop-aws:3.2.0,org.apache.commons:commons-csv:1.8 \
-  --jars /s3datasource/target/scala-2.12/s3datasource_2.12-0.1.0.jar \
+  --packages com.github.scopt:scopt_2.12:4.0.0-RC2 \
+  --jars /build/extra_jars/*,/s3datasource/target/scala-2.12/s3datasource_2.12-0.1.0.jar,/build/h2-latest.jar \
   /tpch/tpch-spark/target/scala-2.12/spark-tpc-h-queries_2.12-1.0.jar $@
+    #--packages com.github.scopt:scopt_2.12:4.0.0-RC2,com.amazonaws:aws-java-sdk:1.11.853,org.apache.hadoop:hadoop-aws:3.2.0,org.apache.commons:commons-csv:1.8 \
 else
   # --master local
-  docker exec -it sparkmaster spark-submit --master local[4] \
+  docker exec -it sparkmaster spark-submit --master local[1] \
   --conf "log4j.rootCategory=WARN,console" \
   --conf "ivy.shared.default.root=/build/ivy_jars" \
   --conf "spark.driver.extraClassPath=/build/extra_jaars/*" \
@@ -29,7 +30,7 @@ else
   --conf "spark.eventLog.dir=/build/spark-events" \
   --conf "spark.driver.extraJavaOptions=-classpath /conf/:/build/spark-3.1.0/jars/*:/examples/scala/target/scala-2.12/" \
   --packages com.github.scopt:scopt_2.12:4.0.0-RC2 \
-  --jars /build/extra_jars/*,/s3datasource/target/scala-2.12/s3datasource_2.12-0.1.0.jar \
+  --jars /build/extra_jars/*,/s3datasource/target/scala-2.12/s3datasource_2.12-0.1.0.jar,/build/h2-latest.jar \
   /tpch/tpch-spark/target/scala-2.12/spark-tpc-h-queries_2.12-1.0.jar $@
 fi
 
