f53634033c6e54d73442ef2186a0cf5b91061214
Robert Foley <robfoley972@gmail.com>
2021-01-04 17:50:41 -0500

    Updates to tpch test to log results to file.

--- benchmark/tpch/run_all_script.sh
+++ benchmark/tpch/run_all_script.sh
@@ -1,10 +1,18 @@
 #! /bin/bash
+ 
+CURRENT_TIME=$(date "+%Y-%m-%d_%H-%M-%S")
+ 
+TEST_RESULTS="tpch_results_$CURRENT_TIME.csv"
+echo "Output file is: $TEST_RESULTS" 
 
-./run_tpch.py -t 1-22 -a "--test tblFile" --veth
-./run_tpch.py -t 1-22 -a "--test tblS3 --s3Filter"
-./run_tpch.py -t 1-22 -a "--test tblS3 --s3Filter --s3Project"
-./run_tpch.py -t 1-14,16-21 -a "--test tblS3 --s3Select"
+./run_tpch.py -t 1-22 -a "--test tblFile" --veth --results $TEST_RESULTS
+./run_tpch.py -t 1-22 -a "--test tblS3 --s3Filter" --results $TEST_RESULTS
+./run_tpch.py -t 1-22 -a "--test tblS3 --s3Filter --s3Project" --results $TEST_RESULTS
+./run_tpch.py -t 1-14,16-21 -a "--test tblS3 --s3Select" --results $TEST_RESULTS
 
-./run_tpch.py -t 1-22 -a "--test tblPartS3 --s3Filter"
-./run_tpch.py -t 1-22 -a "--test tblPartS3 --s3Filter --s3Project"
-./run_tpch.py -t 1-14,16-21 -a "--test tblPartS3 --s3Select"
+./run_tpch.py -t 1-22 -a "--test tblPartS3 --s3Filter" --results $TEST_RESULTS
+./run_tpch.py -t 1-22 -a "--test tblPartS3 --s3Filter --s3Project" --results $TEST_RESULTS
+./run_tpch.py -t 1-14,16-21 -a "--test tblPartS3 --s3Select" --results $TEST_RESULTS
+
+./run_tpch.py -t 1-22 -a "--test tblHdfs" --results $TEST_RESULTS
+./run_tpch.py -t 1-22 -a "--test v2CsvHdfs" --results $TEST_RESULTS
