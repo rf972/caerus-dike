ae1b4f702cf4a41297479fa94e51f450ca35e452
Robert Foley <robfoley972@gmail.com>
2021-01-25 17:30:41 -0500

    Added example code for testing hdfs. Also added build_test.sh, and updated run_example.sh for working with the new dikehdfs.

--- spark/examples/scala/build.sbt
+++ spark/examples/scala/build.sbt
@@ -6,8 +6,6 @@ scalaVersion := "2.12.10"
 val sparkVersion = "3.0.0"
 
 libraryDependencies ++= Seq(
- "org.apache.spark" %% "spark-core" % sparkVersion,
- "org.apache.spark" %% "spark-sql" % sparkVersion,
   "org.scala-lang" % "scala-library" % scalaVersion.value % "compile",
   "org.scalatest" % "scalatest_2.12" % "3.2.2",
 )
--- spark/examples/scala/run_example.sh
+++ spark/examples/scala/run_example.sh
@@ -3,18 +3,21 @@
 if [ "$1" == "debug" ]; then
   echo "Debugging"
   docker exec -it sparkmaster spark-submit --master local \
-  --class io.s3.datasource.tests.S3DatasourceTests \
+  --class com.github.datasource.tests.DatasourceHdfsTests \
   --conf "spark.jars.ivy=/build/ivy" \
-  --conf "spark.driver.extraJavaOptions=-classpath /conf/:/build/spark-3.1.0/jars/*:/examples/scala/target/scala-2.12/ -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=172.18.0.3:5005" \
-  --packages org.apache.httpcomponents:httpcore:4.4.11,org.apache.httpcomponents:httpclient:4.5.11,com.amazonaws:aws-java-sdk:1.11.853,org.apache.hadoop:hadoop-aws:3.2.0,org.apache.commons:commons-csv:1.8 \
-  --jars /pushdown-datasource/target/scala-2.12/pushdown-datasource_2.12-0.1.0.jar \
+  --conf "spark.driver.extraJavaOptions=-classpath /conf/:/build/spark-3.2.0/jars/*:/examples/scala/target/scala-2.12/ -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=172.18.0.3:5005" \
+  --packages com.github.scopt:scopt_2.12:4.0.0-RC2 \
+  --jars /dikeHDFS/client/dikeclient/target/dikeclient-1.0-jar-with-dependencies.jar,/build/extra_jars/*,/pushdown-datasource/target/scala-2.12/pushdown-datasource_2.12-0.1.0.jar,/build/downloads/h2-1.4.200.jar\
   /examples/scala/target/scala-2.12/spark-examples_2.12-1.0.jar minioserver
+
+#  --packages org.apache.httpcomponents:httpcore:4.4.11,org.apache.httpcomponents:httpclient:4.5.11,com.amazonaws:aws-java-sdk:1.11.853,org.apache.hadoop:hadoop-aws:3.2.0,org.apache.commons:commons-csv:1.8 \
+#  --jars /pushdown-datasource/target/scala-2.12/pushdown-datasource_2.12-0.1.0.jar \
 else
   docker exec -it sparkmaster spark-submit --master local \
-  --class io.s3.datasource.tests.S3DatasourceTests \
+  --class com.github.datasource.tests.DatasourceHdfsTests \
   --conf "spark.jars.ivy=/build/ivy" \
-  --conf "spark.driver.extraJavaOptions=-classpath /conf/:/build/spark-3.1.0/jars/*:/examples/scala/target/scala-2.12/" \
-  --packages org.apache.httpcomponents:httpcore:4.4.11,org.apache.httpcomponents:httpclient:4.5.11,com.amazonaws:aws-java-sdk:1.11.853,org.apache.hadoop:hadoop-aws:3.2.0,org.apache.commons:commons-csv:1.8 \
-  --jars /pushdown-datasource/target/scala-2.12/pushdown-datasource_2.12-0.1.0.jar \
+  --conf "spark.driver.extraJavaOptions=-classpath /conf/:/build/spark-3.2.0/jars/*:/examples/scala/target/scala-2.12/" \
+  --packages com.github.scopt:scopt_2.12:4.0.0-RC2 \
+  --jars /dikeHDFS/client/dikeclient/target/dikeclient-1.0-jar-with-dependencies.jar,/build/extra_jars/*,/pushdown-datasource/target/scala-2.12/pushdown-datasource_2.12-0.1.0.jar,/build/downloads/h2-1.4.200.jar\
   /examples/scala/target/scala-2.12/spark-examples_2.12-1.0.jar minioserver
 fi
