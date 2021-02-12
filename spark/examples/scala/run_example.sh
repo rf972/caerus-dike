8694d2fead8ecf7c16039999206a14c5cf09e56c
Robert Foley <robfoley972@gmail.com>
2021-02-12 13:31:02 -0500

    Latest updates for dependencies to test scripts.  Minor updates to test code.

--- spark/examples/scala/run_example.sh
+++ spark/examples/scala/run_example.sh
@@ -5,7 +5,7 @@ if [ "$1" == "debug" ]; then
   docker exec -it sparkmaster spark-submit --master local \
   --class com.github.datasource.tests.DatasourceHdfsTests \
   --conf "spark.jars.ivy=/build/ivy" \
-  --conf "spark.driver.extraJavaOptions=-classpath /conf/:/build/spark-3.2.0/jars/*:/examples/scala/target/scala-2.12/ -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=172.18.0.3:5005" \
+  --conf "spark.driver.extraJavaOptions=-classpath /conf/:/build/spark-3.2.0/jars/*:/examples/scala/target/scala-2.12/ -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=172.18.0.2:5005" \
   --packages com.github.scopt:scopt_2.12:4.0.0-RC2 \
   --jars /dikeHDFS/client/dikeclient/target/dikeclient-1.0-jar-with-dependencies.jar,/build/extra_jars/*,/pushdown-datasource/target/scala-2.12/pushdown-datasource_2.12-0.1.0.jar,/build/downloads/h2-1.4.200.jar\
   /examples/scala/target/scala-2.12/spark-examples_2.12-1.0.jar minioserver
--- spark/examples/scala/run_jdbc_unit_test.sh
+++ spark/examples/scala/run_jdbc_unit_test.sh
@@ -6,7 +6,7 @@ if [ "$1" == "debug" ]; then
   --class org.apache.spark.sql.jdbc.example.UnitTest \
   --conf "spark.jars.ivy=/build/ivy" \
   --conf "spark.shuffle.service.enabled=false" \
-  --conf "spark.driver.extraJavaOptions=-classpath /conf/:/build/spark-3.1.0/jars/*:/examples/scala/target/scala-2.12/ -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=172.18.0.3:5005" \
+  --conf "spark.driver.extraJavaOptions=-classpath /conf/:/build/spark-3.2.0/jars/*:/examples/scala/target/scala-2.12/ -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=172.18.0.2:5005" \
   --jars /build/h2-latest.jar \
   /examples/scala/target/scala-2.12/spark-examples_2.12-1.0.jar minioserver
 else
@@ -14,8 +14,8 @@ else
   --class org.apache.spark.sql.jdbc.example.UnitTest \
   --conf "spark.jars.ivy=/build/ivy" \
   --conf "spark.shuffle.service.enabled=false" \
-  --conf "spark.driver.extraJavaOptions=-classpath /conf/:/build/spark-3.1.0/jars/*:/examples/scala/target/scala-2.12/" \
-  --jars /build/h2-latest.jar \
+  --conf "spark.driver.extraJavaOptions=-classpath /conf/:/build/spark-3.2.0/jars/*:/examples/scala/target/scala-2.12/" \
+  --jars /build/extra_jars/*,/build/downloads/h2-1.4.200.jar\
   /examples/scala/target/scala-2.12/spark-examples_2.12-1.0.jar minioserver
 #  --jars /build/sqlite-jdbc-3.32.3.2.jar,/build/mariadb-java-client-2.6.2.jar \
 fi
