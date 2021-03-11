4537066d13086484f7bb1b43725f0a708750e871
Robert Foley <robfoley972@gmail.com>
2021-03-11 15:19:22 -0500

    Removed unused files.

--- mc/docker/build_dockers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker build -f Dockerfile.mc_build -t mc_build .
-
-docker build -f Dockerfile.mc_run -t mc_run .
-
-docker build -f Dockerfile.mc_debug -t mc_debug .
-
-echo "Done"
--- minio/docker/build_dockers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker build -f Dockerfile.minio_build_server -t minio_build_server .
-
-docker build -f Dockerfile.minio_run_server -t minio_run_server .
-
-docker build -f Dockerfile.minio_debug_server -t minio_debug_server .
-
-echo "Done"
