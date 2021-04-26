1346a823723bda8ffafec79024477f25a0711bae
Robert Foley <robfoley972@gmail.com>
2021-04-26 13:58:42 -0400

    Changes for S3 in scripting.  Includes new tests for S3.

--- benchmark/tpch/run_tpch.sh
+++ benchmark/tpch/run_tpch.sh
@@ -38,7 +38,7 @@ if [ ${DEBUG} == "YES" ]; then
   --conf "spark.jars.ivy=/build/ivy" \
   --conf "spark.driver.maxResultSize=20g" \
   --conf "spark.driver.memory=20g" \
-  --conf "spark.driver.extraJavaOptions=-classpath /conf/:/build/spark-3.2.0/jars/*:/examples/scala/target/scala-2.12/ -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=172.19.0.3:5005" \
+  --conf "spark.driver.extraJavaOptions=-classpath /conf/:/build/spark-3.2.0/jars/*:/examples/scala/target/scala-2.12/ -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=172.19.0.2:5005" \
   --packages com.github.scopt:scopt_2.12:4.0.0-RC2 \
   --jars /dikeHDFS/client/ndp-hdfs/target/ndp-hdfs-1.0.jar,/build/extra_jars/*,/pushdown-datasource/target/scala-2.12/pushdown-datasource_2.12-0.1.0.jar,/build/downloads/h2-1.4.200.jar \
   /tpch/tpch-spark/target/scala-2.12/spark-tpc-h-queries_2.12-1.0.jar $@ --workers ${WORKERS}
@@ -55,8 +55,8 @@ else
   --conf "spark.eventLog.enabled=true" \
   --conf "spark.eventLog.dir=/build/spark-events" \
   --conf "spark.driver.extraJavaOptions=-classpath /conf/:/build/spark-3.2.0/jars/*:/examples/scala/target/scala-2.12/" \
-  --packages com.github.scopt:scopt_2.12:4.0.0-RC2 \
-  --jars /dikeHDFS/client/ndp-hdfs/target/ndp-hdfs-1.0.jar,/build/extra_jars/*,/pushdown-datasource/target/scala-2.12/pushdown-datasource_2.12-0.1.0.jar,/build/downloads/h2-1.4.200.jar\
+  --packages com.github.scopt:scopt_2.12:4.0.0-RC2,org.apache.commons:commons-csv:1.8,org.apache.hadoop:hadoop-aws:3.2.2\
+  --jars /dikeHDFS/client/ndp-hdfs/target/ndp-hdfs-1.0.jar,/build/extra_jars/aws-java-sdk-1.11.1001.jar,/build/extra_jars/*,/pushdown-datasource/target/scala-2.12/pushdown-datasource_2.12-0.1.0.jar,/build/downloads/h2-1.4.200.jar\
   /tpch/tpch-spark/target/scala-2.12/spark-tpc-h-queries_2.12-1.0.jar $@ --workers ${WORKERS}
 fi
 
