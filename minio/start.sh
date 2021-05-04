a7ad63ede3661ceb6e468c5ed01f08f2ffc73be4
Robert Foley <robfoley972@gmail.com>
2021-05-04 08:57:07 -0400

    Added new scripts to start and stop a minio server. We use this for compatibility testing and perf comparisons.

---
+++ minio/start.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+set -e
+printf "\nStarting minio S3 gateway in detached mode ...\n"
+DAEMONIZE="-d=true"
+DOCKER_CMD="docker run --rm=true ${DAEMONIZE} \
+            -e \"MINIO_ROOT_USER=admin\" \
+            -e \"MINIO_ROOT_PASSWORD=admin123\" \
+            --name minioserver --hostname minioserver \
+            --network dike-net \
+            minio/minio:RELEASE.2021-01-16T02-19-44Z gateway hdfs hdfs://dikehdfs:9000"
+
+eval "$DOCKER_CMD" || (echo "*** failed start of minio docker $?" ; exit 1)
+echo "minio started successfully"
