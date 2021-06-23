882c8512d6d7cb04315d291bf08bb808ab62271a
Robert Foley <robfoley972@gmail.com>
2021-06-23 14:27:00 -0400

    Updates for parquet support.  New script to help view parquet metadata and help generate parquet test files.

--- benchmark/tpch/run_tpch.sh
+++ benchmark/tpch/run_tpch.sh
@@ -37,12 +37,14 @@ if [ ${DEBUG} == "YES" ]; then
   --class main.scala.TpchQuery \
   --conf "spark.jars.ivy=/build/ivy" \
   --conf "spark.driver.maxResultSize=20g" \
-  --conf "spark.driver.memory=20g" \
-  --conf "spark.driver.extraJavaOptions=-classpath /conf/:/build/spark-3.2.0/jars/*:/examples/scala/target/scala-2.12/ -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=172.18.0.4:5005" \
+  --conf "spark.driver.memory=2g" \
+  --conf "spark.executor.memory=2g" \
+  --conf "spark.driver.extraJavaOptions=-classpath /conf/:/build/spark-3.2.0/jars/*:/examples/scala/target/scala-2.12/ -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=172.21.0.3:5005" \
   --packages com.github.scopt:scopt_2.12:4.0.0-RC2,ch.cern.sparkmeasure:spark-measure_2.12:0.17 \
   --jars /build/downloads/spark-sql-macros_2.12.10_0.1.0-SNAPSHOT.jar,/dikeHDFS/client/ndp-hdfs/target/ndp-hdfs-1.0.jar,/build/extra_jars/*,/pushdown-datasource/target/scala-2.12/pushdown-datasource_2.12-0.1.0.jar,/build/downloads/h2-1.4.200.jar \
   /tpch/tpch-spark/target/scala-2.12/spark-tpc-h-queries_2.12-1.0.jar $@ --workers ${WORKERS}
     #--packages com.github.scopt:scopt_2.12:4.0.0-RC2,com.amazonaws:aws-java-sdk:1.11.853,org.apache.hadoop:hadoop-aws:3.2.0,org.apache.commons:commons-csv:1.8 \
+    #  --conf "spark.sql.parquet.enableVectorizedReader=false" \
 else
   docker exec -it sparkmaster spark-submit --master local[$WORKERS] \
   --conf "ivy.shared.default.root=/build/ivy_jars" \
@@ -51,8 +53,11 @@ else
   --conf "spark.jars.ivy=/build/ivy" \
   --conf "spark.driver.maxResultSize=20g" \
   --conf "spark.sql.broadcastTimeout=10000000" \
-  --conf "spark.driver.memory=20g" \
+  --conf "spark.driver.memory=2g" \
+  --conf "spark.executor.memory=2g" \
   --conf "spark.eventLog.enabled=true" \
+  --conf "spark.memory.offHeap.enabled=true" \
+  --conf "spark.memory.offHeap.size=2g" \
   --conf "spark.eventLog.dir=/build/spark-events" \
   --conf "spark.driver.extraJavaOptions=-classpath /conf/:/build/spark-3.2.0/jars/*:/examples/scala/target/scala-2.12/" \
   --packages com.github.scopt:scopt_2.12:4.0.0-RC2,ch.cern.sparkmeasure:spark-measure_2.12:0.17 \
