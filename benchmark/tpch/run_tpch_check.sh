ba75a16cc09ffba27d6b65fd5f51c7cce2e55538
Robert Foley <robfoley972@gmail.com>
2021-07-13 11:41:24 -0400

    Added more tests to run_tpch_check.sh.  This includes parquet support as well as s3, and hdfs csv support.

--- benchmark/tpch/run_tpch_check.sh
+++ benchmark/tpch/run_tpch_check.sh
@@ -7,6 +7,11 @@ echo "results file is: $RESULTS_FILE"
 TESTS="-t 1-22"
 #EXTRA_DEFAULT="--dry_run --results $RESULTS_FILE"
 EXTRA_DEFAULT="--results $RESULTS_FILE"
+init_test() {
+  ./build_tbl.sh ../../data
+  ./run_tpch.sh --mode initCsv
+  ./run_tpch.sh --mode initParquet
+}
 hdfs_baseline() {
     local WORKERS="--workers $1"
     local EXTRA="$2"
@@ -16,12 +21,38 @@ hdfs_baseline() {
 hdfs_all() {
     local WORKERS="--workers $1"
     local EXTRA="$2"
-    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol ndphdfs $EXTRA"
+    #./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol ndphdfs $EXTRA"
     #./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol ndphdfs --pushProject $EXTRA"
     #./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol ndphdfs --pushFilter --pushProject $EXTRA"
     ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol ndphdfs --pushdown $EXTRA"
     #./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol hdfs $EXTRA"
     #./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol webhdfs $EXTRA"
+}
+hdfs_csv_all() {
+    local WORKERS="--workers $1"
+    local EXTRA="$2 --format csv"
+    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol ndphdfs $EXTRA"
+    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol ndphdfs --pushProject $EXTRA"
+    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol ndphdfs --pushFilter --pushProject $EXTRA"
+    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol ndphdfs --pushdown $EXTRA"
+    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol hdfs $EXTRA"
+    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol webhdfs $EXTRA"
+}
+hdfs_parquet() {
+    local WORKERS="--workers $1"
+    local EXTRA="$2 --format parquet"
+    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol hdfs --pushFilter --pushProject $EXTRA"
+}
+hdfs_parquet_all() {
+    local WORKERS="--workers $1"
+    local EXTRA="$2 --format parquet"
+    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol hdfs --outputFormat parquet $EXTRA"
+    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol hdfs --outputFormat parquet --pushProject $EXTRA"
+    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol hdfs --outputFormat parquet --pushFilter --pushProject $EXTRA"
+    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol ndphdfs --outputFormat csv $EXTRA"
+    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol ndphdfs --outputFormat csv --pushProject $EXTRA"
+    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol ndphdfs --outputFormat csv --pushProject --pushFilter $EXTRA"
+    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol ndphdfs --outputFormat csv --pushdown $EXTRA"
 }
 hdfs_check() {
     local EXTRA=$1
@@ -55,10 +86,17 @@ s3_file() {
     shift
     local EXTRA="$*"
 
-    #./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "--ds spark --protocol file $EXTRA"
-    #./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol s3 $EXTRA"
-    #./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol s3 --pushProject $EXTRA"
-    #./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol s3 --pushFilter --pushProject $EXTRA"
+    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol s3 --pushdown $EXTRA"
+}
+s3_file_all() {
+    local WORKERS="--workers $1"
+    shift
+    local EXTRA="$*"
+
+    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds spark --protocol file $EXTRA"
+    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol s3 $EXTRA"
+    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol s3 --pushProject $EXTRA"
+    ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol s3 --pushFilter --pushProject $EXTRA"
     ./run_tpch.py $WORKERS $TESTS $EXTRA_DEFAULT -a "-ds ndp --protocol s3 --pushdown $EXTRA"
 }
 s3() {
@@ -76,6 +114,10 @@ if [ "$1" == "hdfscheck" ]; then
   hdfs_check "--check"
 elif [ "$1" == "hdfsshort" ]; then
   hdfs_short "--format csv"
+elif [ "$1" == "hdfsparquetcheck" ]; then
+  hdfs_all 1 "--format parquet --outputFormat csv --check"
+elif [ "$1" == "hdfsparquetperf" ]; then
+  hdfs_parquet 4 "--format parquet --outputFormat parquet"
 elif [ "$1" == "hdfsperf" ]; then
   hdfs_perf ""
 elif [ "$1" == "s3miniocheck" ]; then
@@ -88,11 +130,15 @@ elif [ "$1" == "s3perf" ]; then
   s3 ""
 elif [ "$1" == "s3short" ]; then
   s3_short "--format csv"
+elif [ "$1" == "allhdfs" ]; then
+  hdfs_parquet_all 1 "--check"
+  hdfs_csv_all 1 "--check"
+elif [ "$1" == "alls3" ]; then
+  s3_file_all 1 "--check"
 elif [ "$1" == "all" ]; then
-  hdfs ""
-  hdfs "--check"
-  s3 ""
-  s3 "--check"
+  hdfs_csv_all 1 "--check"
+  hdfs_parquet_all 1 "--check"
+  s3_file_all 1 "--check"
 else
   echo "missing test: provide test to run (hdfscheck,hdfsperf,hdfsshort,s3check,s3perf)"
 fi
