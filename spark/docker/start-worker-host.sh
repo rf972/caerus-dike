b211efd8ab0dfaae2b20b311c2fff4477ce2fa24
Robert Foley <robfoley972@gmail.com>
2021-09-02 16:58:09 -0400

    Added new scripts for spark worker docker to be started with docker host networking.

---
+++ spark/docker/start-worker-host.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+source docker/setup.sh
+
+mkdir -p "${ROOT_DIR}/volume/logs"
+rm -f "${ROOT_DIR}/volume/logs/worker*.log"
+
+mkdir -p "${ROOT_DIR}/volume/worker/status"
+rm -f "${ROOT_DIR}/volume/status/WORKER*"
+
+CMD="${DOCKER_HOME_DIR}/bin/start-worker-host.sh"
+RUNNING_MODE="daemon"
+
+WORKERS=1
+if [ "$#" -ge 1 ] ; then
+  WORKERS=$1
+fi
+ADD_HOSTS="$(cat worker.config)"
+if [ "$#" -ge 2 ] ; then
+  ADD_HOSTS="$2"
+fi
+echo "Add Hosts: $ADD_HOSTS"
+
+if [ $RUNNING_MODE = "interactive" ]; then
+  DOCKER_IT="-i -t"
+fi
+#  --cpuset-cpus="16-19" \
+#  --network dike-net ${REMOTE_STORAGE}\
+DOCKER_RUN="docker run ${DOCKER_IT} --rm -p 8081:8081 \
+  --expose 7012 --expose 7013 --expose 7014 --expose 7015 --expose 8881 \
+  --name sparkworker \
+  --network host ${ADD_HOSTS} \
+  -e SPARK_CONF_DIR=/conf \
+      -e SPARK_WORKER_INSTANCES=1 \
+      -e SPARK_WORKER_CORES=$WORKERS \
+      -e SPARK_WORKER_MEMORY=8g \
+      -e SPARK_WORKER_PORT=8881 \
+      -e SPARK_WORKER_WEBUI_PORT=8081 \
+      -e SPARK_PUBLIC_DNS=localhost \
+  --mount type=bind,source="$(pwd)"/spark,target=/spark \
+  --mount type=bind,source=$(pwd)/build,target=/build \
+  -v ${ROOT_DIR}/conf/worker:/conf \
+  -v /nvme-part/caerus/rf972-caerus-dike/data:/tpch-data \
+  -v ${ROOT_DIR}/build/.m2:${DOCKER_HOME_DIR}/.m2 \
+  -v ${ROOT_DIR}/build/.gnupg:${DOCKER_HOME_DIR}/.gnupg \
+  -v ${ROOT_DIR}/build/.sbt:${DOCKER_HOME_DIR}/.sbt \
+  -v ${ROOT_DIR}/build/.cache:${DOCKER_HOME_DIR}/.cache \
+  -v ${ROOT_DIR}/build/.ivy2:${DOCKER_HOME_DIR}/.ivy2 \
+  -v ${ROOT_DIR}/volume/status:/opt/volume/status \
+  -v ${ROOT_DIR}/volume/logs:/opt/volume/logs \
+  -v ${ROOT_DIR}/bin/:${DOCKER_HOME_DIR}/bin \
+  -e RUNNING_MODE=${RUNNING_MODE} \
+  -u ${USER_ID} \
+  spark-run-${USER_NAME} ${CMD}"
+
+
+if [ $RUNNING_MODE = "interactive" ]; then
+  eval "${DOCKER_RUN}"
+else
+  eval "${DOCKER_RUN}" &
+  while [ ! -f "${ROOT_DIR}/volume/status/SPARK_WORKER_STATE" ]; do
+    sleep 1
+  done
+
+  cat "${ROOT_DIR}/volume/status/SPARK_WORKER_STATE"
+fi
+
